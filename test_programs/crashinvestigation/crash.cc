d245aafa5bf34ab36daf434866a4b18e097becf7
Maciek Gajewski <maciej.gajewski0@gmail.com>
2015-12-17 15:41:52 +0100

    crash investigation completed

--- test_programs/crashinvestigation/crash.cc
+++ test_programs/crashinvestigation/crash.cc
@@ -6,25 +6,21 @@
 int main(int argc, char** argv)
 {
 	if (argc < 2) return 1;
-	::initscr();
-
 	CXIndex clang_idx = clang_createIndex(0, 0);
-	CXTranslationUnit clang_tu = nullptr;
 
-//	clang_parseTranslationUnit2(
-//			clang_idx,
-//			/*argv[1]*/ nullptr,
-//			argv, argc, // cmndline
-//			nullptr, 0, // unsaved data
-//			clang_defaultEditingTranslationUnitOptions(),
-//			&clang_tu);
+	::initscr();
+	::trace(TRACE_MAXIMUM);
 
-	clang_tu = clang_parseTranslationUnit(
+	::_tracef("before calling parse");
+
+	CXTranslationUnit clang_tu2 = clang_parseTranslationUnit(
 			clang_idx,
 			/*argv[1]*/ nullptr,
 			argv, argc, // cmndline
 			nullptr, 0, // unsaved data
 			clang_defaultEditingTranslationUnitOptions());
+
+	::_tracef("after calling parse");
 
 
 	::endwin();
