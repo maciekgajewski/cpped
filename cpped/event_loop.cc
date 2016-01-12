fdf881db39c4c64124489bb3653c087e6e3ce05c
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-01-12 20:44:58 +0100

    event loop

---
+++ cpped/event_loop.cc
@@ -0,0 +1,137 @@
+#include "event_loop.hh"
+
+#include <stdexcept>
+#include <cassert>
+#include <vector>
+
+#include <sys/epoll.h>
+#include <unistd.h>
+
+namespace cpped {
+
+class epoll_observer
+{
+public:
+
+	epoll_observer()
+	{
+		epoll_fd_ = ::epoll_create1(0);
+		if (epoll_fd_ < 0)
+			throw std::runtime_error("Failed ot create epoll");
+	}
+
+	~epoll_observer()
+	{
+		::close(epoll_fd_);
+	}
+
+	void observe_for_readablity(int fd, epoll_data_t data)
+	{
+		epoll_event event;
+		event.data = data;
+		event.events = EPOLLIN;
+
+		int r = ::epoll_ctl(
+			epoll_fd_,
+			EPOLL_CTL_ADD,
+			fd,
+			&event);
+
+		if (r < 0)
+			throw std::runtime_error("Error adding file to epoll");
+
+		files_++;
+	}
+
+	void remove(int fd)
+	{
+	int r = ::epoll_ctl(
+			epoll_fd_,
+			EPOLL_CTL_DEL,
+			fd,
+			nullptr);
+
+		if (r < 0)
+			throw std::runtime_error("Error removing file to epoll");
+		assert(files_ > 0);
+		files_--;
+	}
+
+	void wait(int ms, std::vector<epoll_event>& events)
+	{
+		events.resize(files_);
+
+		int r = ::epoll_wait(
+			epoll_fd_,
+			events.data(),
+			files_,
+			ms);
+
+		if (r < 0)
+			throw std::runtime_error("epoll_wait failed");
+
+		assert(r < files_);
+		events.resize(r);
+	}
+
+private:
+
+	int epoll_fd_;
+	unsigned files_ = 0;
+};
+
+static thread_local epoll_observer* current_instance = nullptr;
+
+event_loop::event_loop()
+{
+	if (current_instance)
+		throw std::logic_error("Only onve instance of event_loop per thread allowed");
+
+	current_instance = new epoll_observer;
+}
+
+event_loop::~event_loop()
+{
+	delete current_instance;
+	current_instance = nullptr;
+}
+
+void event_loop::run()
+{
+	assert(current_instance);
+	run_ = true;
+
+	while(run_)
+	{
+		std::vector<epoll_event> events;
+		current_instance->wait(10 /*ms*/, events);
+
+		for(const epoll_event& event : events)
+		{
+			observed_file* f = static_cast<observed_file*>(event.data.ptr);
+			f->handler_();
+		}
+	}
+}
+
+observed_file::observed_file(int fd, const observed_file::handler_t& handler)
+	: fd_(fd), handler_(handler)
+{
+	assert(handler_);
+	if (!current_instance)
+		throw std::logic_error("Event loop not availabale");
+
+	epoll_data_t data;
+	data.ptr = this;
+	current_instance->observe_for_readablity(fd, data);
+}
+
+observed_file::~observed_file()
+{
+	assert(current_instance);
+	current_instance->remove(fd_);
+}
+
+
+
+}
---
+++ cpped/event_loop.hh
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <functional>
+
+namespace cpped {
+
+// Simple event loop, observing number if file descriptors for readability
+class event_loop
+{
+public:
+
+	event_loop();
+	~event_loop();
+
+	static event_loop* get_current();
+
+	// starts the event loop. Wil lreturn only after stop is called
+	void run();
+
+	// interrupts event loop
+	void stop() { run_ = false; }
+
+private:
+	bool run_;
+};
+
+// File registration. When createdm will register file in event loop and cal handler whenever file becomes readable
+class observed_file
+{
+public:
+	using handler_t = std::function<void()>;
+
+	observed_file(int fd, const handler_t& handler);
+	~observed_file();
+
+private:
+
+	int fd_;
+	handler_t handler_;
+
+	friend class event_loop;
+};
+
+}
